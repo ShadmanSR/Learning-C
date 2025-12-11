8f10334b3b783fee8f3d54ebd110e1af0e3507fd
ShadmanSR <rupon2305@gmail.com>
2025-12-11 15:12:14 +0600

    Add files via upload

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -6,6 +6,8 @@ int main() {
     int quantity = 0;
     char currency = '$';
     float total = 0.0f;
+    char payment[10] = "";
+
 
     printf("What item would you like to buy? ");
     fgets(item, sizeof(item), stdin);
@@ -14,17 +16,21 @@ int main() {
     scanf("%f", &price);
 
 
-    printf("How many items  would he/she like to buy? ");
+    printf("How many items would you like to buy? ");
     scanf("%d", &quantity);
 
     total = price * quantity    ;
     printf("\nYou have bought %d %s/s \n", quantity , item);
-    printf("%c%.2f", currency , total);
+    printf("\nThe total is %c%.2f", currency , total);
+
+    printf("\nWill you pay with cash or card?");
+    scanf("%s", &payment);
+ return 0;
 } 
 
 
 
 
-  
 
+  
 
